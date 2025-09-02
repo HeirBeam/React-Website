a4860690dc9ba7a4ac2de515f0eed5c299f4247d
Ibraheem Sharif <ibraheemsharif@gmail.com>
2025-09-02 12:57:46 +0100

    initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <main style={{ padding:24 }}>
+            <h1>About</h1>
+            <p>Created in about folder in page.tsx file</p>
+            </main>
+    );
+}
